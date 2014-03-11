b67658a6e1fca9ad33e9353750aaf51505d517dd
Michael Hughes <MichaelRyanHughes@gmail.com>
2014-03-11 10:43:13 -0700

    Add security to Works

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,17 +1,15 @@
 class WorksController < ApplicationController
+  # NOTE: add helper method to return permitted params private method. 
+  before_filter :authenticate_user!, only: [:create, :update, :destroy]
   
   def index
-    #@works = Work.all.sort_by{:created_at}.reverse
-    @works = Work.all.order(:created_at).reverse
+    @works = Work.order(created_at: :desc)
   end
 
-  def create                                        #add a new work.
-    #puts "hello" 
-    #puts params[:work]
-    Work.create params[:work].permit(:name, :url, :vimeo_id)   #makes a new db entry with 
-    #redirect_to :back                              #accepted data from form
-    redirect_to '/admin'                            #takes user back to admin page
-  end                                               #showing all works including the new one.
+  def create                                                    #add a new work.
+    Work.create params[:work].permit(:name, :url, :vimeo_id)    #makes a new db entry with 
+    redirect_to '/admin'                                        #takes user back to admin page
+  end                                                           #showing all works including the new one.
 
   def update
     @work = Work.find params[:id]
